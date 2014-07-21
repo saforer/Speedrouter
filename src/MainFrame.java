2e9b8b26918fd41f84d051082c049362230acc32
saforer <saforer@gmail.com>
2014-07-21 07:33:01 -0400

    "Real" Init upload, Simple stuff so far so I can always return to home.

--- src/MainFrame.java
+++ src/MainFrame.java
@@ -9,10 +9,15 @@ import javax.swing.border.EmptyBorder;
 public class MainFrame extends JFrame {
 
 	private JPanel contentPane;
+	
+	private PathPanel pathPanel;
+	private MapPanel mapPanel;
+	private ResPanel resPanel;
 
 	public MainFrame() {
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		setBounds(100, 100, 450, 300);
+		setTitle("Speedtest");
 		contentPane = new JPanel();
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 		contentPane.setLayout(new BorderLayout(0, 0));
@@ -25,22 +30,14 @@ public class MainFrame extends JFrame {
 		splitPane_1.setOrientation(JSplitPane.VERTICAL_SPLIT);
 		splitPane.setLeftComponent(splitPane_1);
 		
-		JPanel pathPanel = new PathPanel();
-		splitPane_1.setLeftComponent(pathPanel);
+		pathPanel = new PathPanel();
+		resPanel = new ResPanel();
+		mapPanel = new MapPanel();
 		
-		JPanel resPanel = new ResPanel();
-		splitPane_1.setRightComponent(resPanel);
-		
-		JPanel mapPanel = new MapPanel();
+		splitPane_1.setLeftComponent(pathPanel);		
+		splitPane_1.setRightComponent(resPanel);		
 		splitPane.setRightComponent(mapPanel);
 		
-		if (((PathPanel) pathPanel).getActiveNode() == null) {
-			this.setName("Speedrouter");
-		} else {
-			this.setName(((PathPanel) pathPanel).getActiveNode());
-		}
-		
 		
 		}
-
 }
